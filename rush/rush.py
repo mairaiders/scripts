b6402d62d9944b38d2ba4a2e9f1ed069ac7370bb
mairaiders <raidconversations@gmail.com>
2019-11-27 21:47:57 +0300

    Args added, asynh is optional

--- rush/rush.py
+++ rush/rush.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
-import vk, time, json, pickle, os.path, sys, signal
+import vk, time, json, pickle, os.path, sys, signal, random
 import urllib.request as urlreq
+import argparse
 from threading import Thread
 from termcolor import colored as C
 
@@ -9,12 +10,12 @@ import functions
 
 ACCESS_KEYS_FILE='access_keys.txt'
 ACTIONS_FILE='actions.txt'
-PEER_IDS_FILE = 'peer_ids.dat'
-MAX_CYCLES = 100000 # 100 or 500
+PEER_IDS_FILE='peer_ids.dat'
+MAX_CYCLES=1000 # 100 or 500
 API_VERSION='5.103'
-DELAY=0.5
+DELAY=0.7
 LONGPOLLWAIT=10
-COMMENT = "#"
+COMMENT="#"
 
 def parse_access_keys(file):    
 	access_keys = []
@@ -52,7 +53,7 @@ def parse_actions(file):
 
 # Global variable needed for stop threads
 done = False
-def wait_for_invite(bot):	
+def wait_for_invite(bot, spam=False, act=None):	
 	print('  {} wait for invite'.format(bot['name']))
 	lps = bot['api'].groups.getLongPollServer(group_id=bot['group_id'])
 	while not done:	
@@ -69,7 +70,11 @@ def wait_for_invite(bot):
 			i['object']['message']['action']['type'] == 'chat_invite_user' and \
 			i['object']['message']['action']['member_id'] == -bot['group_id']:
 				bot['peer_id'] = i['object']['message']['peer_id']
-				return 
+				if spam and act:
+					while True:
+						perform(bot, act)
+						time.sleep(DELAY)
+				return
 				
 def wait_for_invites(bots):
 	def status():
@@ -115,7 +120,6 @@ def wait_for_invites(bots):
 	print(C('Bots are ready to spam', 'yellow', attrs=['bold']))
 	
 	return [i for i in bots if 'peer_id' in i]
-
 		
 def intersection(l1, l2, item):
 	l1_names = set((i[item] for i in l1))
@@ -145,27 +149,27 @@ def connect_peer_ids_with_bots(bots, peer_ids, option='remove'):
 	elif option == 'partial':
 		return bots
 	
+def perform(bot, act):
+	msg = act['function'](*act['args'])
+	req = bot['api'].messages.send
+	rand = random.getrandbits(64)
+	try:
+		if act['msg_type'] == 'text':
+			req(peer_id=bot['peer_id'], random_id=rand, message=msg)			
+		elif act['msg_type'] == 'attachment':
+			req(peer_id=bot['peer_id'], random_id=rand, attachment=msg)		
+	except vk.exceptions.VkAPIError as e:
+		print(C(e, 'red', attrs=['bold']))
+		return
+	print(C('{} prints: '.format(bot['name']), 'yellow', attrs=['bold']) + '{}'.format(msg))
+	
 def spam(bots, acts):
-	# len(bots) == len(acts) is true
-	rnd = 0
 	cycles = 0
 	msgs_sent = 0
-	done = False
-	while not done:
+	while True:
 		for j in acts:
 			i = next((x for x in bots if x['name'] == j['name']), None)
-			msg = j['function'](*j['args'])
-			req = i['api'].messages.send
-			try:
-				if j['msg_type'] == 'text':
-					req(peer_id=i['peer_id'], random_id=rnd+1, message=msg)			
-				elif j['msg_type'] == 'attachment':
-					req(peer_id=i['peer_id'], random_id=rnd+2, attachment=msg)		
-			except vk.exceptions.VkAPIError as e:
-				continue
-			msgs_sent += 1
-			print(C('{} prints: '.format(i['name']), 'yellow', attrs=['bold']) + '{}'.format(msg))
-			rnd ^= 1
+			perform(i, j)
 		print(C('{} cycles left, {} messages sent'.format(MAX_CYCLES-cycles-1, msgs_sent), 'yellow', attrs=['bold']))
 		time.sleep(DELAY)
 		cycles += 1
@@ -174,9 +178,26 @@ def spam(bots, acts):
 			if ans in 'Yy':
 				cycles = 0
 			else:
-				done = True
+				return
 	
-def main():
+def wait_for_invites_and_spam(bots, acts):
+	print(C('Waiting for invites...', 'yellow', attrs=['bold']))	
+	for i in bots:	
+		act = next((x for x in acts if x['name'] == i['name']), None)
+		thread = Thread(target=wait_for_invite, args=(i, True, act))
+		thread.start()
+
+def main():	
+	global PEER_IDS_FILE
+	
+	parser = argparse.ArgumentParser(epilog='Written by mairaiders <raidconversations@gmail.com>')
+	parser.add_argument('-a', '--asynh', action='store_true', help='send messages immediately after invitation')
+	parser.add_argument('-V', '--version', action='version', version='rush.py 0.3')
+	parser.add_argument('-f', '--file', default=PEER_IDS_FILE, dest='file', help='set file contains peer ids')
+	
+	args = parser.parse_args()
+	
+	PEER_IDS_FILE = args.file
 	print(C('Loading access keys...', 'yellow', attrs=['bold']))
 	keys = parse_access_keys(ACCESS_KEYS_FILE)
 	
@@ -196,13 +217,16 @@ def main():
 			wait = False
 		elif ans in 'Pp':
 			bots = connect_peer_ids_with_bots(bots, load_peer_ids(PEER_IDS_FILE), 'partial')
-						
-	if wait:
+	
+	if wait and args.asynh:
+		wait_for_invites_and_spam(bots, acts)
+	elif wait:
 		bots = wait_for_invites(bots)		
 		save_peer_ids(bots, PEER_IDS_FILE)
-			
-	intersection(bots, acts, 'name')	
-	spam(bots, acts)	
+
+	if not args.asynh:
+		intersection(bots, acts, 'name')	
+		spam(bots, acts)	
 	
 if __name__ == '__main__':
 	try:
